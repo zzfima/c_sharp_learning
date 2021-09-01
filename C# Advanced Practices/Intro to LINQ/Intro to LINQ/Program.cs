07faf5d531400a0f647ba6e97dc060c233e44964
zfima <zzfima@gmail.com>
2021-09-01 13:01:57 +0300

    add

--- C# Advanced Practices/Intro to LINQ/Intro to LINQ/Program.cs
+++ C# Advanced Practices/Intro to LINQ/Intro to LINQ/Program.cs
@@ -7,14 +7,27 @@ namespace Intro_to_LINQ
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 5, 4, 1, 3, 6, 7, 8, 9 };
-
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
             var lowNums = from n in numbers
                           where n < 5
                           select n;
+            foreach (var n in lowNums)
+                Console.Write(n + " ");
 
-            foreach (int n in lowNums)
-                Console.WriteLine(n);
+            Console.WriteLine();
+            string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            var shortDigits = digits.Where((digit, index) => digit.Length < index);
+            foreach (var n in shortDigits)
+                Console.Write(n + " ");
+
+            Console.WriteLine();
+            string[] strings = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            var textNums = from n in numbers
+                           select strings[n];
+            foreach (var n in textNums)
+                Console.Write(n + " ");
+
+
         }
     }
 }
