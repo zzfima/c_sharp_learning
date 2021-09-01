170fe18646b15deba45af3e6a8c7efcfd49a412e
zfima <zzfima@gmail.com>
2021-09-01 12:38:56 +0300

    start

---
+++ C# Advanced Practices/Intro to LINQ/Intro to LINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Intro_to_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 1, 3, 6, 7, 8, 9 };
+
+            var lowNums = from n in numbers
+                          where n < 5
+                          select n;
+
+            foreach (int n in lowNums)
+                Console.WriteLine(n);
+        }
+    }
+}
