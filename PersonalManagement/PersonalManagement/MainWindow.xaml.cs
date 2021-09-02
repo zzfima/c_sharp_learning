032779ff2a5ff79833a11db1eb727f6c84d87331
zfima <zzfima@gmail.com>
2021-09-02 11:34:37 +0300

    start

---
+++ PersonalManagement/PersonalManagement/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace PersonalManagement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<MyObject> myObjects;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myObjects = new ObservableCollection<MyObject>()
+            {
+                new MyObject(){C1 = "Jimmy", C2 = "Yang"},
+                new MyObject(){C1 = "Marvin", C2 = "Guo"},
+                new MyObject(){C1 = "Franklin", C2 = "Chen"}
+            };
+
+            this.dgContent.ItemsSource = myObjects;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            MyObject myObject = new MyObject() { C1 = this.txtC1.Text, C2 = this.txtC2.Text };
+            myObjects.Add(myObject);
+        }
+    }
+}
---
+++ PersonalManagement/PersonalManagement/MyObject.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonalManagement
+{
+    internal class MyObject
+    {
+        public string C1 { get; set; }
+        public string C2 { get; set; }
+    }
+}
