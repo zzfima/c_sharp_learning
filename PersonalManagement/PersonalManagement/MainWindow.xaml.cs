b32f40e4ad538afe9f9a25fa18aa5b2ac8ac3e36
zzfima <zzfima@gmail.com>
2021-09-03 14:57:41 +0300

    dispose locator pdf

--- PersonalManagement/PersonalManagement/MainWindow.xaml.cs
+++ PersonalManagement/PersonalManagement/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using Implementations;
 using Interfaces;
+using iText.Kernel.Colors;
 using Microsoft.Extensions.DependencyInjection;
 using Models;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Linq;
 using System.Windows;
 
 namespace PersonalManagement
@@ -14,7 +16,7 @@ namespace PersonalManagement
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<PersonExport> _personsObservable;
+        ObservableCollection<PersonExport> _personExportObservable;
         ServiceProvider _serviceProvider;
         public MainWindow()
         {
@@ -23,11 +25,11 @@ namespace PersonalManagement
             Bootstrap();
 
             IEnumerable<Person> personsList = _serviceProvider.GetService<IRepository<Person>>().ReadAll();
-            _personsObservable = new ObservableCollection<PersonExport>();
+            _personExportObservable = new ObservableCollection<PersonExport>();
             foreach (Person p in personsList)
-                _personsObservable.Add(new PersonExport { Person = p, IsExport = false });
-            this.dgContent.ItemsSource = _personsObservable;
-            _personsObservable.CollectionChanged += Changed;
+                _personExportObservable.Add(new PersonExport { Person = p, IsExport = false });
+            this.dgContent.ItemsSource = _personExportObservable;
+            _personExportObservable.CollectionChanged += Changed;
         }
 
         private void Bootstrap()
@@ -35,7 +37,10 @@ namespace PersonalManagement
             IServiceCollection services = new ServiceCollection();
 
             services.AddSingleton<IRepository<Person>>(new ProductXMLRepository(Properties.Settings.Default.pathToXML));
-            services.AddSingleton<ITextSharpExporter>(new TextSharpPDFExporter(Properties.Settings.Default.pathToPDF));
+            services.AddTransient<ITextSharpExporter, TextSharpPDFExporter>();
+
+            services.AddTransient<ITextSharpExporter>(
+                x => ActivatorUtilities.CreateInstance<TextSharpPDFExporter>(x, Properties.Settings.Default.pathToPDF));
 
             _serviceProvider = services.BuildServiceProvider();
         }
@@ -59,17 +64,47 @@ namespace PersonalManagement
             };
 
             _serviceProvider.GetService<IRepository<Person>>().Add(person);
-            _personsObservable.Add(new PersonExport { Person = person, IsExport = false });
+            _personExportObservable.Add(new PersonExport { Person = person, IsExport = false });
         }
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
             PersonExport personExportToDelete = this.dgContent.SelectedItem as PersonExport;
             _serviceProvider.GetService<IRepository<Person>>().Remove(personExportToDelete.Person);
-            _personsObservable.Remove(this.dgContent.SelectedItem as PersonExport);
+            _personExportObservable.Remove(this.dgContent.SelectedItem as PersonExport);
         }
 
         private void btnExport_Click(object sender, RoutedEventArgs e)
         {
+            using (ITextSharpExporter exporter = _serviceProvider.GetService<ITextSharpExporter>())
+            {
+                List<PersonExport> personsToExport = (from p in this._personExportObservable
+                                                      where p.IsExport
+                                                      select p).ToList();
+
+                exporter.AddHeader("Persons List", iText.Layout.Properties.TextAlignment.CENTER, 80);
+                exporter.AddHorizontalLineSeparator();
+
+                string[] header = { "FirstName", "LastName", "DateOfBirth", "Gender" };
+                string[,] body = new string[personsToExport.Count, 4];
+                int rowCnt = 0;
+
+                foreach (PersonExport personExport in personsToExport)
+                {
+                    body[rowCnt, 0] = personExport.Person.FirstName;
+                    body[rowCnt, 1] = personExport.Person.LastName;
+                    body[rowCnt, 2] = personExport.Person.DateOfBirth.ToShortDateString();
+                    body[rowCnt, 3] = personExport.Person.Gender.ToString();
+
+                    rowCnt++;
+                }
+
+                exporter.AddTable(4, true,
+                    ColorConstants.LIGHT_GRAY, iText.Layout.Properties.TextAlignment.CENTER, header,
+                    ColorConstants.ORANGE, iText.Layout.Properties.TextAlignment.LEFT, body);
+
+                exporter.AddHorizontalLineSeparator();
+            }
         }
+
     }
 }
