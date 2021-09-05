baf49af715f81a5ac7bac28ca96b82a8d6d63f41
zzfima <zzfima@gmail.com>
2021-09-05 06:23:40 +0300

    extract bootstrap

---
+++ PersonalManagement/PersonalManagement/Bootstrap.cs
@@ -0,0 +1,26 @@
+﻿using Implementations;
+using Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Models;
+using System;
+
+namespace PersonalManagement
+{
+    public class Bootstrap
+    {
+        public ServiceProvider Build()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<IRepository<Person>>(new ProductXMLRepository(Properties.Settings.Default.pathToXML));
+            string dirPath = @"Exports\";
+
+            if (!System.IO.Directory.Exists(dirPath))
+                System.IO.Directory.CreateDirectory(dirPath);
+
+            services.AddTransient<ITextSharpExporter>(x => ActivatorUtilities.CreateInstance<TextSharpPDFExporter>(x, dirPath + DateTime.Now.Ticks.ToString() + "_" + Properties.Settings.Default.pathToPDF));
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
--- PersonalManagement/PersonalManagement/MainWindow.xaml.cs
+++ PersonalManagement/PersonalManagement/MainWindow.xaml.cs
@@ -1,9 +1,5 @@
-﻿using Implementations;
-using Interfaces;
-using Microsoft.Extensions.DependencyInjection;
-using Models;
+﻿using Microsoft.Extensions.DependencyInjection;
 using PersonalManagement.ViewModel;
-using System;
 using System.Windows;
 
 namespace PersonalManagement
@@ -19,24 +15,10 @@ namespace PersonalManagement
         public MainWindow()
         {
             InitializeComponent();
-            Bootstrap();
+            Bootstrap bootstrap = new Bootstrap();
+            _serviceProvider = bootstrap.Build();
             _vMPerson = new VMMainView(_serviceProvider);
             this.DataContext = _vMPerson;
-        }
-
-        private void Bootstrap()
-        {
-            IServiceCollection services = new ServiceCollection();
-
-            services.AddSingleton<IRepository<Person>>(new ProductXMLRepository(Properties.Settings.Default.pathToXML));
-            string dirPath = @"Exports\";
-
-            if (!System.IO.Directory.Exists(dirPath))
-                System.IO.Directory.CreateDirectory(dirPath);
-
-            services.AddTransient<ITextSharpExporter>(x => ActivatorUtilities.CreateInstance<TextSharpPDFExporter>(x, dirPath + DateTime.Now.Ticks.ToString() + "_" + Properties.Settings.Default.pathToPDF));
-
-            _serviceProvider = services.BuildServiceProvider();
         }
     }
 }
