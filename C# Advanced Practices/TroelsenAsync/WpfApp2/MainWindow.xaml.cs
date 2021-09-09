9bfa107e3bd4155643d16d177081e9c29e7c3279
zfima <zzfima@gmail.com>
2021-09-09 16:31:34 +0300

    add awaitable wpf

---
+++ C# Advanced Practices/TroelsenAsync/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public delegate Task<int> BinaryOp(int x, int y);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BinaryOp binaryOp = Add;
+            int res = await binaryOp(2, 5);
+            txt.Text = res.ToString();
+        }
+
+        static async Task<int> Add(int x, int y)
+        {
+            Console.WriteLine($"Add() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Run(() => Thread.Sleep(3000));
+            return x + y + DateTime.Now.Millisecond;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            txt1.Text = DateTime.Now.Millisecond.ToString();
+        }
+    }
+}
