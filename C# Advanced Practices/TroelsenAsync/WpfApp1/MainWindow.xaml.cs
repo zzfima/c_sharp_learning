2744c49e6da83107eba1998bf2c5d33701b104c1
zfima <zzfima@gmail.com>
2021-09-09 12:26:37 +0300

    add wpf proj

---
+++ C# Advanced Practices/TroelsenAsync/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public delegate int BinaryOp(int x, int y);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BinaryOp binaryOp = Add;
+            int res = binaryOp(2, 5);
+            txt.Text = res.ToString();
+        }
+
+        static int Add(int x, int y)
+        {
+            Console.WriteLine($"Add() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(3000);
+            return x + y;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            txt1.Text = DateTime.Now.Millisecond.ToString();
+        }
+    }
+}
