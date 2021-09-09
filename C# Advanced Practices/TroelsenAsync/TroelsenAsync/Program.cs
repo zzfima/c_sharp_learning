7b89fcbaf437d6e430e008d0fd51f99b96ae179b
zfima <zzfima@gmail.com>
2021-09-09 12:13:34 +0300

    rfct

--- C# Advanced Practices/TroelsenAsync/TroelsenAsync/Program.cs
+++ C# Advanced Practices/TroelsenAsync/TroelsenAsync/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace TroelsenAsync
 {
     class Program
     {
+        public delegate int BinaryOp(int x, int y);
         static void Main(string[] args)
         {
+            Console.WriteLine($"Main() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            BinaryOp binaryOp = Add;
+            int res = binaryOp(2, 5);
+            Console.WriteLine($"Result of 2+5 is {res}");
+
+            IAsyncResult asyncResult = binaryOp.BeginInvoke(3, 6, (callback) => 
+            {
+                Console.WriteLine($"Async callback invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            }, null);
+
+            Console.WriteLine($"IAsyncResult IsCompleted: {asyncResult.IsCompleted}");
+            Console.WriteLine($"Result is: {binaryOp.EndInvoke(asyncResult)}");
+
+            Console.ReadLine();
+        }
+
+        static int Add(int x, int y)
+        {
+            Console.WriteLine($"Add() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(3000);
+            return x + y;
         }
     }
 }
