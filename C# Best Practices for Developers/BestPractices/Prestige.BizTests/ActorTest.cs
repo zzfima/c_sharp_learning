1ed2db3e52ce93ad56ed8fb0d17ae06c7a84e16d
zzfima <zzfima@gmail.com>
2021-09-10 19:37:17 +0300

    rfct

---
+++ C# Best Practices for Developers/BestPractices/Prestige.BizTests/ActorTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prestige.Biz;
+
+namespace Prestige.BizTests
+{
+    [TestClass]
+    public class ActorTest
+    {
+        [TestMethod]
+        public void TestOccupation()
+        {
+            //Arrange
+            Actor actor = new Actor();
+
+            //Act
+            string occupation = actor.GetOccupation();
+
+            //Assert
+            Assert.AreEqual(occupation, "Actor");
+        }
+    }
+}
