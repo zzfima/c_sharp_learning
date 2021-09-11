ea8294e2a1a550da0aaaf871142bb03048a0160b
zzfima <zzfima@gmail.com>
2021-09-11 09:22:12 +0300

    add singleton

---
+++ C# Best Practices for Developers/BestPractices/Prestige.Common/Policy.cs
@@ -0,0 +1,34 @@
+﻿namespace Prestige.Common
+{
+    public class Policy
+    {
+        static private Policy _instance;
+        private string _level;
+
+        public string Level
+        {
+            get => _level;
+            set
+            {
+                _level = "Level is: " + value;
+            }
+        }
+
+        private Policy()
+        {
+
+        }
+
+        static public Policy Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Policy();
+
+                return _instance;
+            }
+        }
+
+    }
+}
---
+++ C# Best Practices for Developers/BestPractices/Prestige.CommonTests/PolicyTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prestige.Common;
+
+namespace Prestige.CommonTests
+{
+    [TestClass]
+    public class PolicyTest
+    {
+        [TestMethod]
+        public void TestLevel()
+        {
+            //Arrange
+            string expected = "Level is: Low Load";
+
+            //Act
+            Policy.Instance.Level = "High Load";
+            Policy.Instance.Level = "Low Load";
+            string actual = Policy.Instance.Level;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
