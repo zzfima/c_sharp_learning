bd80c100dfdace42fe282da702ccbb32df7ec495
zfima <zzfima@gmail.com>
2021-09-12 15:28:34 +0300

    add salary calc

---
+++ C# Test Driven Development/SalaryCalculatorTestProject/SalaryCalculatorTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SalaryCalculatorTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SalaryCalculatorTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a86bf7f3-f01a-4448-a607-7ffc034aea5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ C# Test Driven Development/SalaryCalculatorTestProject/SalaryCalculatorTestProject/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SalaryCalculatorTestProject
+{
+    [TestClass]
+    public class UnitTestSalaryCalculator
+    {
+        [TestMethod]
+        public void TestGetHourly()
+        {
+            //Arrange
+            SalaryCalculator salaryCalculator = new SalaryCalculator();
+            float expectedPerHourSalary = 48.08f;
+            float annualSalary = 100006.40f;
+
+            //Act
+            float actualPerHourSalary = salaryCalculator.GetPerHourSalary(annualSalary);
+
+            //Assert
+            Assert.AreEqual(expectedPerHourSalary, actualPerHourSalary);
+        }
+
+        [TestMethod]
+        public void TestGetAnnual()
+        {
+            //Arrange
+            SalaryCalculator salaryCalculator = new SalaryCalculator();
+            float expectedAnnualSalary = 100006.40f;
+            float perHourSalary = 48.08f;
+
+            //Act
+            float actualAnnualSalary = salaryCalculator.GetAnnualSalary(perHourSalary);
+
+            //Assert
+            Assert.AreEqual(expectedAnnualSalary, actualAnnualSalary);
+        }
+    }
+}
