452b3f32172bdf19fd0de97283bdd691e14f773d
zfima <zzfima@gmail.com>
2021-09-13 10:11:42 +0300

    rfct

---
+++ C# Test Driven Development/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using System.Collections.Generic;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int hours = 55, wage = 70;
+
+            var mock = new Mock<Utils>();
+            mock.Setup(m => m.GetMockEmployees()).Returns(() => new List<Employee> { new Contractor(), new Employee() });
+
+            List<Employee> employees = mock.Object.GetMockEmployees();
+
+            foreach (var e in employees)
+            {
+                e.CalculateWeeklySalary(hours, wage);
+            }
+        }
+    }
+}
---
+++ C# Test Driven Development/Polymorphism/Polymorphism/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polymorphism
+{
+    internal class Utils
+    {
+        public virtual List<Employee> GetMockEmployees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
