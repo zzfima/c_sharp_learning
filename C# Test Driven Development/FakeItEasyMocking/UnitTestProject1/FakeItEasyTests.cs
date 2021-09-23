13cb073e4889e2aee0d1ae6f25b6b520775b81fd
zzfima <zzfima@gmail.com>
2021-09-23 19:51:55 +0300

    finish

--- C# Test Driven Development/FakeItEasyMocking/UnitTestProject1/FakeItEasyTests.cs
+++ C# Test Driven Development/FakeItEasyMocking/UnitTestProject1/FakeItEasyTests.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Transportation;
 
 namespace UnitTestProject1
 {
@@ -9,12 +11,38 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            var distributor = A.Fake<Distributor>();
+            var car = A.Fake<Car>(x => x.WithArgumentsForConstructor(() => new Car("WB 82112")));
+            var petrolStation = A.Fake<PetrolStation>();
+            var transaction = A.Fake<Transaction>();
+            var vehicle = A.Fake<IVehicle>();
+
+            decimal totalPrice = transaction.GetFuelTotalPrice(distributor, 4);
+
+            A.CallTo(() => distributor.GetFuelPrice()).MustHaveHappenedOnceExactly();
+            A.CallTo(() => transaction.Finish()).DoesNothing();
+
+            A.CallTo(() => vehicle.CheckEngineStatus()).Throws<NotImplementedException>();
+
+            try
+            {
+                vehicle.CheckEngineStatus();
+            }
+            catch (Exception ex)
+            {
+                var exceptionType = ex.GetType();
+                Assert.AreEqual(exceptionType, typeof(NotImplementedException));
+            }
         }
 
         [TestInitialize]
         public void TestInitialize()
         {
-
+            var distributor = A.Fake<Distributor>();
+            var car = A.Fake<Car>(x => x.WithArgumentsForConstructor(() => new Car("WB 82112")));
+            var petrolStation = A.Fake<PetrolStation>();
+            var transaction = A.Fake<Transaction>();
+            var vehicle = A.Fake<IVehicle>();
         }
     }
 }
