5523913d8a7c1d48a64754ea8a73ab5d0e2feedc
zfima <zzfima@gmail.com>
2021-10-10 12:41:22 +0300

    start game chess

---
+++ ChessGame/ChessGame/UnitTestChessGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestChessGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestChessGame")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41a90c2a-42c9-410c-990c-05c18aa1af10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ChessGame/ChessGame/UnitTestChessGame/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+
+namespace UnitTestChessGame
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodInit()
+        {
+            ChessGame chessGame = new ChessGame();
+            Square<IEnumerable> squares = chessGame.GetCurrentSquare(ChessFigures.King, FigureColor.White);
+            Square square = squares.FirstOrDefault();
+
+            Assert.AreEqual(square.rank, SquareRanks.One);
+            Assert.AreEqual(square.file, SquareFiles.E);
+        }
+    }
+}
