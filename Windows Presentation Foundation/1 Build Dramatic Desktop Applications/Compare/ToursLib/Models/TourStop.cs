3f212533215a9ec1b39569ff25de8263e3e0206b
zzfima <zzfima@gmail.com>
2021-10-15 13:41:33 +0300

    rfct

--- Windows Presentation Foundation/1 Build Dramatic Desktop Applications/Compare/ToursLib/Models/TourStop.cs
+++ Windows Presentation Foundation/1 Build Dramatic Desktop Applications/Compare/ToursLib/Models/TourStop.cs
@@ -1,26 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace TailorMadeTours.Models {
-public	class TourStop {
-		public int StopNumber { get; set; }
-		public string Name { get; set; }
-		public string Latitude { get; set; }
-		public string Longitude { get; set; }
-		public string Description { get; set; }
-		public string Phone { get; set; }
-		public string ImageUri { get; set; }
-		public List<BusyTime> BusyTimes { get; set; }
-		public bool Selected { get; set; }
-		public int EstimatedMinutes { get; set; }
+namespace TailorMadeTours.Models
+{
+    public class TourStop
+    {
+        public int StopNumber { get; set; }
+        public string Name { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public string ImageUri { get; set; }
+        public List<BusyTime> BusyTimes { get; set; }
+        public bool Selected { get; set; }
+        public int EstimatedMinutes { get; set; }
 
-	}
-	public class BusyTime {
-		public int Hour { get; set; }
-		public int Rank { get; set; }
+    }
+    public class BusyTime
+    {
+        public int Hour { get; set; }
+        public int Rank { get; set; }
 
-	}
+    }
 }
