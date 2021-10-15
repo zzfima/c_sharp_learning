9b850988d8ac326ba6aead408c8fadc4a227f93b
zzfima <zzfima@gmail.com>
2021-10-15 21:17:40 +0300

    start new proj

---
+++ Windows Presentation Foundation/1 Build Dramatic Desktop Applications/TailorMadeTours/TailorMadeTours/Models/TourStop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace TailorMadeTours.Models
+{
+    public class TourStop
+    {
+        public int StopNumber { get; set; }
+        public string Name { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public string ImageUri { get; set; }
+        public List<BusyTime> BusyTimes { get; set; }
+        public bool Selected { get; set; }
+        public int EstimatedMinutes { get; set; }
+
+    }
+    public class BusyTime
+    {
+        public int Hour { get; set; }
+        public int Rank { get; set; }
+
+    }
+}
