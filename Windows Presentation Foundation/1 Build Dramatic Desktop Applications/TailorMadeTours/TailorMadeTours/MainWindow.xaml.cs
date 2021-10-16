c666d6e792e7cc5026dc088672b5baa1a67dfcaa
zzfima <zzfima@gmail.com>
2021-10-16 09:28:40 +0300

    add data template

--- Windows Presentation Foundation/1 Build Dramatic Desktop Applications/TailorMadeTours/TailorMadeTours/MainWindow.xaml.cs
+++ Windows Presentation Foundation/1 Build Dramatic Desktop Applications/TailorMadeTours/TailorMadeTours/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 
 namespace TailorMadeTours
 {
@@ -7,9 +8,16 @@ namespace TailorMadeTours
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static List<MyEntities> MyItems = new List<MyEntities>()
+        {
+            new MyEntities { Name = "Murka", Age = 4 },
+            new MyEntities { Name = "Bloha4", Age = 4 },
+            new MyEntities { Name = "Dilona", Age = 4 },
+        };
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = MyItems;
         }
     }
 }
---
+++ Windows Presentation Foundation/1 Build Dramatic Desktop Applications/TailorMadeTours/TailorMadeTours/MyEntities.cs
@@ -0,0 +1,8 @@
+﻿namespace TailorMadeTours
+{
+    public class MyEntities
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
