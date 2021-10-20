fc9dd86fcb2153231c250d9c1277ba06c6c59b5a
zfima <zzfima@gmail.com>
2021-10-20 08:28:42 +0300

    rfct

---
+++ Windows Presentation Foundation/2 Layout/UseLetterBox/UseLetterBox/CustomPanel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UseLetterBox
+{
+    public class CustomPanel : Panel
+    {
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            var v = this.Children;
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Size sumSize = new Size();
+
+            foreach (UIElement item in Children)
+            {
+                item.Measure(availableSize);
+                sumSize.Width += item.DesiredSize.Width;
+                sumSize.Height += item.DesiredSize.Height;
+            }
+
+            return sumSize;
+        }
+    }
+}
