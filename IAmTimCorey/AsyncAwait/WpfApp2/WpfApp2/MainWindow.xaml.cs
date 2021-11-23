0e39309771d55a853b1092fe7e173a48dc48c559
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2021-11-23 16:11:36 +0200

    rfct

--- IAmTimCorey/AsyncAwait/WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ IAmTimCorey/AsyncAwait/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace WpfApp2
@@ -10,6 +12,8 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _isWorkingStop = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -18,10 +22,13 @@ namespace WpfApp2
 
         private void ButtonBase_OnClick_Sync(object sender, RoutedEventArgs e)
         {
+            _isWorkingStop = false;
             ResultsWindow.Text = String.Empty;
             Stopwatch stopwatch = Stopwatch.StartNew();
             for (int i = 0; i < 5; i++)
             {
+                if (_isWorkingStop)
+                    return;
                 LongWork();
                 ProgressBar.Value = i * 25;
             }
@@ -30,20 +37,55 @@ namespace WpfApp2
             ResultsWindow.Text += $"Total execution time: { elapsedMs }";
         }
 
-        private void ButtonBase_OnClick_Async(object sender, RoutedEventArgs e)
+        private async void ButtonBase_OnClick_Async(object sender, RoutedEventArgs e)
         {
-
+            _isWorkingStop = false;
+            ResultsWindow.Text = String.Empty;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < 5; i++)
+            {
+                if (_isWorkingStop)
+                    break;
+                await Task.Run(LongWork);
+                ProgressBar.Value = i * 25;
+            }
+            stopwatch.Stop();
+            long elapsedMs = stopwatch.ElapsedMilliseconds;
+            ResultsWindow.Text += $"Total execution time: { elapsedMs }";
         }
 
-        private void ButtonBase_OnClick_Parallel(object sender, RoutedEventArgs e)
+        private async void ButtonBase_OnClick_Parallel(object sender, RoutedEventArgs e)
         {
+            _isWorkingStop = false;
+            ResultsWindow.Text = String.Empty;
+            Stopwatch stopwatch = Stopwatch.StartNew();
 
+            List<Task> tasks = new List<Task>();
+            
+            for (int i = 0; i < 5; i++)
+            {
+                if (_isWorkingStop)
+                    break;
+                tasks.Add(Task.Run(LongWork));
+                ProgressBar.Value = i * 25;
+            }
+
+            await Task.WhenAll(tasks);
+
+            stopwatch.Stop();
+            long elapsedMs = stopwatch.ElapsedMilliseconds;
+            ResultsWindow.Text += $"Total execution time: { elapsedMs }";
         }
 
         private void LongWork()
         {
             Thread.Sleep(1000);
-            ResultsWindow.Text += "working... \n";
+            ResultsWindow.Dispatcher.Invoke(()=>ResultsWindow.Text += "working... \n");
+        }
+
+        private void ButtonBase_OnClick_Cancel(object sender, RoutedEventArgs e)
+        {
+            _isWorkingStop = true;
         }
     }
 }
