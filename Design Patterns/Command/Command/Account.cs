3b9637d4e93bb40764d67e5410db7b812034989e
zzfima <zzfima@gmail.com>
2021-11-26 18:08:01 +0200

    start

---
+++ Design Patterns/Command/Command/Account.cs
@@ -0,0 +1,23 @@
+﻿namespace Command
+{
+    internal class Account
+    {
+        private int balance;
+
+        public int Balance => balance;
+
+        public Account() => balance = 0;
+
+        public void Deposit(int money)
+        {
+            if (money < 0) return;
+            balance += money;
+        }
+
+        public void Withdraw(int money)
+        {
+            if(balance - money < 0) return;
+            balance -= money;
+        }
+    }
+}
