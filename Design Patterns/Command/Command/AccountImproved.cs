e9a46ad8f71938ef952a096cfd8897a8d7237a77
zzfima <zzfima@gmail.com>
2021-11-26 20:20:22 +0200

    rfct

---
+++ Design Patterns/Command/Command/AccountImproved.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Command
+{
+    public class AccountImproved
+    {
+        public int Balance { get; set; }
+    }
+
+    public interface IOperation
+    {
+        void Execute();
+        bool IsComplete { get; }
+    }
+
+    public class Withdraw : IOperation
+    {
+        private AccountImproved _accountImproved;
+        private int _money;
+        private bool _isComplete;
+
+        public bool IsComplete { get => _isComplete; }
+
+        public Withdraw(AccountImproved accountImproved, int money)
+        {
+            _accountImproved = accountImproved;
+            _money = money;
+            _isComplete = false;
+        }
+
+        public void Execute()
+        {
+            _accountImproved.Balance += _money;
+            _isComplete = true;
+            Console.WriteLine($"Withdrawn {_money} dollars. Balance: {_accountImproved.Balance}");
+        }
+    }
+
+    public class Deposit : IOperation
+    {
+        private bool _isComplete;
+        private AccountImproved _accountImproved;
+        private int _money;
+
+        public bool IsComplete { get => _isComplete; }
+
+        public Deposit(AccountImproved accountImproved, int money)
+        {
+            _accountImproved = accountImproved;
+            _money = money;
+            _isComplete = false;
+            Console.WriteLine($"Deposit {_money} dollars. Balance: {_accountImproved.Balance}");
+        }
+
+        public void Execute()
+        {
+            _accountImproved.Balance += _money;
+            _isComplete = true;
+        }
+    }
+}
--- Design Patterns/Command/Command/Program.cs
+++ Design Patterns/Command/Command/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Command
+﻿namespace Command
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            AccountImproved accountImproved = new AccountImproved();
+
+            IOperation operation = new Withdraw(accountImproved, 30);
+            operation.Execute();
+
+            operation = new Withdraw(accountImproved, 50);
+            operation.Execute();
+
+            operation = new Deposit(accountImproved, 10);
+            operation.Execute();
         }
     }
 }
