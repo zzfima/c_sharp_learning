0122aafcbf486a51f6f7196372c1f95fc0fa976e
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2021-12-05 08:37:13 +0200

    rfct

---
+++ Рефакторинг унаследованного кода как разорвать зависимости/Dodo.LeagcyCode/Dodo.LegacyCode.WrapClass/LoggingEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dodo.LegacyCode.WrapClass
+{
+    public class LoggingEmployee
+    {
+        private readonly Employee _employee;
+
+        public LoggingEmployee(Employee employee)
+        {
+            _employee = employee;
+        }
+
+        public void Pay(List<TimeEntry> timeEntries, PaymentService paymentService)
+        {
+            _employee.Pay(timeEntries, paymentService);
+
+            LogPayment();
+        }
+
+        private void LogPayment()
+        {
+            // logging payment
+        }
+    }
+}
