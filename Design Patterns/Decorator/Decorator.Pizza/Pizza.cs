1167b9603155bb57ac63826aad6253d64edde354
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2021-12-05 11:35:29 +0200

    decortor method

---
+++ Design Patterns/Decorator/Decorator.Pizza/Pizza.cs
@@ -0,0 +1,22 @@
+﻿namespace Decorator.Pizza
+{
+    public class Pizza
+    {
+        public virtual int Cost() => 50;
+    }
+
+    public class ExtraCheesePizza : Pizza
+    {
+        private readonly Pizza _pizza;
+
+        public ExtraCheesePizza(Pizza pizza)
+        {
+            _pizza = pizza;
+        }
+
+        public override int Cost()
+        {
+            return _pizza.Cost() + 10;
+        }
+    }
+}
---
+++ Design Patterns/Decorator/UnitTestProject1/UnitTestPizza.cs
@@ -0,0 +1,34 @@
+﻿using Decorator.Pizza;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestPizza
+    {
+        [TestMethod]
+        public void TestMethodPizza()
+        {
+            Pizza pizza = new Pizza();
+            Assert.AreEqual(pizza.Cost(), 50);
+        }
+
+        [TestMethod]
+        public void TestMethodExtraCheesePizza()
+        {
+            Pizza pizza = new Pizza();
+            pizza = new ExtraCheesePizza(pizza);
+            Assert.AreEqual(pizza.Cost(), 60);
+        }
+
+        [TestMethod]
+        public void TestMethodExtraExtraExtraCheesePizza()
+        {
+            Pizza pizza = new Pizza();
+            pizza = new ExtraCheesePizza(pizza);
+            pizza = new ExtraCheesePizza(pizza);
+            pizza = new ExtraCheesePizza(pizza);
+            Assert.AreEqual(pizza.Cost(), 80);
+        }
+    }
+}
