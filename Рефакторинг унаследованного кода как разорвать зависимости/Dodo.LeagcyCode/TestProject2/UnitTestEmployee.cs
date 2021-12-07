0349a5fe02cee5b85e2411ff9c9cf9c381201af8
zzfima <zzfima@gmail.com>
2021-12-07 17:42:38 +0200

    rfct

--- Рефакторинг унаследованного кода как разорвать зависимости/Dodo.LeagcyCode/TestProject2/UnitTestEmployee.cs
+++ Рефакторинг унаследованного кода как разорвать зависимости/Dodo.LeagcyCode/TestProject2/UnitTestEmployee.cs
@@ -14,19 +14,22 @@ namespace TestProject2
         [Test]
         public void Test()
         {
-            Employee employee = new Employee();
-            List<TimeEntry> timeEntries = new List<TimeEntry>();
+            for (int i = 0; i < 100000; i++)
+            {
+                Employee employee = new Employee();
+                List<TimeEntry> timeEntries = new List<TimeEntry>();
 
-            PaymentService paymentService = new PaymentService();
-            employee.Pay(timeEntries, paymentService);
+                PaymentService paymentService = new PaymentService();
+                employee.Pay(timeEntries, paymentService);
 
-            LoggingEmployee loggingEmployee = new LoggingEmployee(employee);
-            loggingEmployee.Pay(timeEntries, paymentService);
+                LoggingEmployee loggingEmployee = new LoggingEmployee(employee);
+                loggingEmployee.Pay(timeEntries, paymentService);
 
-            Employee smsEmployee = new SmsEmployee(employee);
-            smsEmployee.Pay(timeEntries, paymentService);
+                Employee smsEmployee = new SmsEmployee(employee);
+                smsEmployee.Pay(timeEntries, paymentService);
 
-            Assert.Pass();
+                Assert.AreEqual(employee, employee);
+            }
         }
     }
 }
