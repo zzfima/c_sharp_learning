6fefdc677a2789ce0f25b22c19f6015e50b33559
zzfima <zzfima@gmail.com>
2021-12-07 22:47:25 +0200

    rfct

---
+++ Рефакторинг унаследованного кода как разорвать зависимости/Dodo.LeagcyCode/TestProject2/CheckMock.cs
@@ -0,0 +1,27 @@
+﻿using MockProject;
+using NUnit.Framework;
+using Moq;
+
+namespace TestProject2
+{
+    public class CheckMock
+    {
+        [Test]
+        public void ShouldBe()
+        {
+            string action="test";
+            Mock<SomeClass> mockSomeClass = new Mock<SomeClass>();
+            
+            mockSomeClass.Setup(mock => mock.DoSomething());
+            
+            MyClass myClass = new MyClass(mockSomeClass.Object);
+            myClass.MyMethod(action);
+            
+            // Explicitly verify each expectation...
+            mockSomeClass.Verify(mock => mock.DoSomething(), Times.Once());
+            
+            // ...or verify everything.
+            // mockSomeClass.VerifyAll();
+        }
+    }
+}
