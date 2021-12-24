82a393be6851821232b94a0a14f69b4500a05f92
zzfima <zzfima@gmail.com>
2021-12-24 11:04:06 +0200

    rfct tests

--- ReSharperTipsTricks/TestProject1/UnitTest1.cs
+++ ReSharperTipsTricks/TestProject1/UnitTest1.cs
@@ -1,21 +1,30 @@
 using FluentAssertions;
 using ReSharperTipsTricks;
+using System.Threading;
 using Xunit;
 
 namespace TestProject1
 {
     public class UnitTest1
     {
-        [Fact]
+        [Fact, Trait("Speed", "Slow")]
         public void Test1()
         {
             MyClass myClass = new MyClass(2);
             myClass.Vl = 7;
+            Thread.Sleep(500);
             myClass.Vl.Should().Be(7);
         }
 
-        [Fact]
+        [Fact, Trait("Speed", "Fast")]
         public void Test2()
+        {
+            MyClass myClass = new MyClass(66);
+            myClass.MyMethod().Should().Be(66);
+        }
+
+        [Fact, Trait("Speed", "Fast")]
+        public void Test3()
         {
             MyClass myClass = new MyClass(66);
             myClass.MyMethod().Should().Be(66);
