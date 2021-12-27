12737f2bc08b35e395fdaab63fc6e8273bfcae31
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2021-12-27 15:01:01 +0200

    rfct

---
+++ Async Programming in C#/ThreadVSThredPool/ThreadVSThredPool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadVSThredPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+
+            worker.StartWorkByTask("kamfora task");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Async Programming in C#/ThreadVSThredPool/ThreadVSThredPool/Worker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadVSThredPool
+{
+    public class Worker
+    {
+        public void StartWorkByThread(string payload)
+        {
+            Console.WriteLine("Start StartWorkByThread");
+            var workingThread = new Thread(() => Work(payload));
+            workingThread.Start();
+            Console.WriteLine(workingThread.Join(1000));
+            Console.WriteLine("Finish StartWorkByThread");
+        }
+
+        public void StartWorkByTask(string payload)
+        {
+            var workingTask = new Task(() => Work(payload));
+
+            workingTask.ContinueWith(t =>
+            {
+                Console.WriteLine("Error");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            workingTask.ContinueWith(t =>
+            {
+                Console.WriteLine("Good");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            workingTask.Start();
+
+            Task.WaitAll(new Task[] { workingTask }, 1000);
+        }
+
+        private void Work(object payload)
+        {
+            int i = 0;
+            Console.WriteLine(payload);
+            Thread.Sleep(2000);
+            //Console.WriteLine(555 / i);
+        }
+    }
+}
