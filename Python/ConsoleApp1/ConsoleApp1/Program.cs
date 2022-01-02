6d38e197b22ad5aa7499ecc1ced098ab2aaa0f09
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-01-02 18:17:39 +0200

    rfct

--- Python/ConsoleApp1/ConsoleApp1/Program.cs
+++ Python/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using IronPython.Hosting;
+﻿using IronPython.Hosting;
+using System;
 
 namespace ConsoleApp1
 {
@@ -11,10 +7,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var py = Python.CreateEngine();
-            py.Execute("print('Oppa')");
+            var engine = Python.CreateEngine();
 
-            py.ExecuteFile(@"..\..\some_python.py");
+            //run script
+            engine.Execute("print('Oppa')");
+
+            //run simple file
+            engine.ExecuteFile(@"..\..\no_func.py");
+
+            var scope = engine.CreateScope();
+
+            //run function without params no returns
+            engine.ExecuteFile(@"..\..\funcs.py", scope);
+            var func_without_params_no_return = scope.GetVariable("func_without_params_no_return");
+            func_without_params_no_return();
+
+            //run function without params returns string
+            engine.ExecuteFile(@"..\..\funcs.py", scope);
+            var func_without_params_return_string = scope.GetVariable("func_without_params_return_string");
+            string res = func_without_params_return_string();
+            Console.WriteLine(res);
+
+            //run function with params returns int
+            engine.ExecuteFile(@"..\..\funcs.py", scope);
+            var func_with_params_return_int = scope.GetVariable("func_with_params_return_int");
+            int r = func_with_params_return_int(66, 77);
+            Console.WriteLine(r);
 
             Console.ReadLine();
         }
---
+++ Python/ConsoleApp1/ConsoleApp1/funcs.py
@@ -0,0 +1,8 @@
+﻿def func_without_params_no_return():
+	print("func without params")
+
+def func_without_params_return_string():
+	return "func without params returns string"
+
+def func_with_params_return_int(a, b):
+	return a+b
