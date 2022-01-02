aa29c0c6dbbcbeda659488fd0357ca0d6f8b9f8e
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-01-02 18:02:03 +0200

    rfct

---
+++ Python/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronPython.Hosting;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var py = Python.CreateEngine();
+            py.Execute("print('Oppa')");
+
+            py.ExecuteFile(@"..\..\some_python.py");
+
+            Console.ReadLine();
+        }
+    }
+}
