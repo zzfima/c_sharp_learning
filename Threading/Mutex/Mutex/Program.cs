9766aac7a616bc5ab5928056219c4454d2764321
zzfima <zzfima@gmail.com>
2022-02-04 19:57:38 +0200

    rfct

---
+++ Threading/Mutex/Mutex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Security.AccessControl;
+
+namespace Mutex
+{
+    class Program
+    {
+        static readonly System.Threading.Mutex m = new System.Threading.Mutex(false, "M2");
+
+        static void Main(string[] args)
+        {
+            m.SetAccessControl(new MutexSecurity("M2", AccessControlSections.Access ));
+            System.Threading.Mutex.TryOpenExisting("M2", out var m12);
+            var ac1 = m.WaitOne();
+            System.Threading.Mutex.TryOpenExisting("M2", out var m22);
+            System.Threading.Mutex.TryOpenExisting("M2", out var m32);
+            Console.ReadLine();
+        }
+    }
+}
