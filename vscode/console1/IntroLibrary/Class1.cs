9fb8d35e913e20e49222e83a484eef3471d97e64
zzfima <zzfima@gmail.com>
2022-02-04 21:44:57 +0200

    rfct

--- vscode/console1/IntroLibrary/Class1.cs
+++ vscode/console1/IntroLibrary/Class1.cs
@@ -1,5 +1,8 @@
 ﻿namespace IntroLibrary;
 public class Class1
 {
-
+    public void Method1()
+    {
+        Console.WriteLine("Hello World!");
+    }
 }
--- vscode/console1/IntroUI/Program.cs
+++ vscode/console1/IntroUI/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace IntroLibrary
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
