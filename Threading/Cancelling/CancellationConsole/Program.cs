04ef364182d86af501d615c8b96522fb06d4c23b
zzfima <zzfima@gmail.com>
2022-02-05 13:50:40 +0200

    start

---
+++ Threading/Cancelling/CancellationConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CancellationConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Hello World!");
+                    Thread.Sleep(1000);
+                }
+            }, token);  // pass the token to the task
+
+            Console.WriteLine("Press any key to stop the task");
+            Console.ReadLine();
+            cts.Cancel();
+            Console.ReadLine();
+        }
+    }
+}
